5b2d3681b6fa4a2fc355a12f9896b835559145ad
Thenewlks <lucas.nascimento269@etec.sp.gov.br>
2021-11-06 22:54:27 -0300

    Init

---
+++ Csharp_proj/Csharp_proj/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_proj
+{
+    class Menu
+    {
+        public string menu()
+        {
+            Console.Clear();
+            Console.WriteLine("1 -Cadastrar Livro")
+            Console.WriteLine("2 -Cadastrar Cliente")
+            Console.WriteLine("1 -Cadastrar Livro")
+            Console.WriteLine("1 -Cadastrar Livro")
+        }
+
+    }
+}
