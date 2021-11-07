78d0675f96a83c8c367f3ff9a934839b8060780b
brunabrits <brunabsilva0409@gmail.com>
2021-11-06 22:28:44 -0300

    init

---
+++ Csharp_proj/Csharp_proj/TipoL.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_proj
+{
+    public class TipoL
+    {
+        public string TRIAL { get; set; }
+    }
+}
