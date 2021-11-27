c6f2525ba029a8552a3a994c6526a4594a6670a2
brunabrits <brunabsilva0409@gmail.com>
2021-11-27 16:48:24 -0300

    commit  bd

---
+++ Csharp_proj/Csharp_proj/MySqlcs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace Csharp_proj
+{
+    class MySqlcs
+    {
+        static void Main(string[] args)
+        {
+            string = sql;
+            MySqlConnection conexao;
+            conexao = new MySqlConnection("server=localhost; database=csharp_proj; uid=root" );
+            try
+            {
+                conexao.Open();
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message.ToString());
+                Console.WriteLine("Deu ruim mané");
+                    }
+             
+
+            sql= "insert into login values ("
+            Console.ReadKey();
+        }
+           
+        }
+    }
+
